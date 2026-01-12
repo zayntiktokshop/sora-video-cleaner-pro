2c5c504d676985ed4529a55698a006de20b53abe
zayntiktokshop <zayn.tiktokshop3689@gmail.com>
2026-01-12 08:21:36 +0800

    Update tailwind.config.js

--- sora-video-cleaner-pro/tailwind.config.js
+++ sora-video-cleaner-pro/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // 这里的 pattern 改为了 **/*，表示扫描所有子文件夹
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
